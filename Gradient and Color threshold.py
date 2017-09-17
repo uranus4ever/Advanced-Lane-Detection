fe5f14aa8b261d28b6d34cbd2c48f056d49dd0e6
Chaoqun Shan <shanchaoqun27@163.com>
2017-09-17 18:22:15 +0800

    Update Color/gradient threshold

---
+++ Gradient and Color threshold.py
@@ -0,0 +1,144 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def Color(img, threshold = (90,255)):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    S = hls[:,:,2]
+    binary = np.zeros_like(S)
+    binary[(S > threshold[0]) & (S <= threshold[1])] = 1
+    return binary
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0,255)):
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Apply x or y gradient with the OpenCV Sobel() function
+    # and take the absolute value
+    if orient == 'x':
+        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
+    if orient == 'y':
+        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
+    # Rescale back to 8 bit integer
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    # Create a copy and apply the threshold
+    binary_output = np.zeros_like(scaled_sobel)
+    # Here I'm using inclusive (>=, <=) thresholds, but exclusive is ok too
+    binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+    # Return the result
+    return binary_output
+
+# Define a function to return the magnitude of the gradient
+# for a given sobel kernel size and threshold values
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Take both Sobel x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Calculate the gradient magnitude
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    # Rescale to 8 bit
+    scale_factor = np.max(gradmag)/255
+    gradmag = (gradmag/scale_factor).astype(np.uint8)
+    # Create a binary image of ones where threshold is met, zeros otherwise
+    binary_output = np.zeros_like(gradmag)
+    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
+
+    # Return the binary image
+    return binary_output
+
+# Define a function to threshold an image for a given range and Sobel kernel
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+    # Grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Calculate the x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Take the absolute value of the gradient direction,
+    # apply a threshold, and create a binary image result
+    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+    binary_output = np.zeros_like(absgraddir)
+    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
+
+    # Return the binary image
+    return binary_output
+
+def Gradient(img, threshold = (0,255)):
+    # Choose a Sobel kernel size
+    ksize = 5  # Choose a larger odd number to smooth gradient measurements
+
+    # Apply each of the thresholding functions
+    gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=threshold)
+    grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=threshold)
+    mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=threshold)
+    dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(-np.pi / 2, np.pi / 2))
+    combined = np.zeros_like(dir_binary)
+    # combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+    combined[(gradx == 1) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+    return combined
+
+
+
+def CombineGradientColor(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+    # Note: img is the undistorted image
+    # img = np.copy(img)
+    # Convert to HLS color space and separate the V channel
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    s_channel = hls[:, :, 2]
+     # Grayscale image
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # Sobel x
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)  # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx)  # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
+
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    # Stack each channel to view their individual contributions in green and blue respectively
+    # This returns a stack of the two binary images, whose components you can see as different colors
+    color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
+
+    # Combine the two binary thresholds
+    combined_binary = np.zeros_like(sxbinary)
+    combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+
+    return combined_binary
+
+
+image = mpimg.imread('./test_images/test5.jpg')
+colorImg = Color(image, threshold=(170,255))
+gradientImg = Gradient(image, threshold=(60,100))
+combine = CombineGradientColor(image, s_thresh=(170,255), sx_thresh=(20,100))
+
+# Plot the result
+
+# plt.figure()
+# plt.imshow(gradientImg, cmap='gray')
+# plt.show()
+
+f, axes = plt.subplots(2, 2, figsize=(24,16))
+(ax1, ax2, ax3, ax4) = axes.ravel()
+
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=20)
+
+ax2.imshow(colorImg, cmap='gray')
+ax2.set_title('S threshold', fontsize=20)
+
+ax3.imshow(gradientImg, cmap='gray')
+ax3.set_title('Sobel x, gradient threshold', fontsize=20)
+
+ax4.imshow(combine, cmap='gray')
+ax4.set_title('Combined S Channel & gradient threshold', fontsize=20)
+
+for ax in axes.ravel():
+    ax.axis('off')
+f.tight_layout()
