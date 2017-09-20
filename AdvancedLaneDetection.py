191e686c710492c7488a39d30263f367bb2ea527
Jack Shan 单超群(PD-PD Engineering) <chaoqun.shan@cheryjaguarlandrover.com>
2017-09-20 13:06:21 +0800

    Update

--- AdvancedLaneDetection.py
+++ AdvancedLaneDetection.py
@@ -365,7 +365,7 @@ else:
 # plt.xlim(0, 1280)
 # plt.ylim(720, 0)
 # plt.show()
-# # Define conversions in x and y from pixels space to meters
+# Define conversions in x and y from pixels space to meters
 # ym_per_pix = 30/720 # meters per pixel in y dimension
 # xm_per_pix = 3.7/700 # meters per pixel in x dimension
 # y_eval = np.max(ploty)
--- Project.py
+++ Project.py
@@ -31,10 +31,10 @@ class Line():
         #y values for detected line pixels
         self.ally = None
 
-    def SanityCheck(self):
-        # check that they have similar curvature
-        # check that they are separated by approx. the right distance horizontally
-        # check that they are roughly parallel
+# SanityCheck:
+#     # check that they have similar curvature
+#     # check that they are separated by approx. the right distance horizontally
+#     # check that they are roughly parallel
 
 def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
     # Note: img is the undistorted image
@@ -67,8 +67,7 @@ def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
 
     return combined_binary
 
-
-def draw():
+def DrawArea():
     # Create an image to draw the lines on
     warp_zero = np.zeros_like(warped).astype(np.uint8)
     color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
@@ -87,12 +86,162 @@ def draw():
     result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
     return result
 
-image = mpimg.imread('./test_images/test5.jpg')
+def Undistort(img, mtx, dist):
+    # Assume input img is mpimg.imread (RGB)
+    # transform to BGR to fit cv2.imread
+    img_BGR = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    dst_img = cv2.undistort(img_BGR, mtx, dist, None, mtx)
+
+    return cv2.cvtColor(dst_img, cv2.COLOR_BGR2RGB)
+
+def warper(img, img_size, src, dst):
+    # Compute and apply perpective transform
+    img_size = tuple(img_size)
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)  # keep same size as input image
+
+    return warped
+
+def draw_lines(points):
+    # Transform float32 array into list
+    points_t = list(map(list, points))
+    lines = [[points_t[0] + points_t[1]], \
+            [points_t[1] + points_t[2]], \
+            [points_t[2] + points_t[3]], \
+            [points_t[3] + points_t[0]]]
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            plt.plot((x1, x2), (y1, y2), color='r', linewidth=2)
+
+def SlidingWindow(img):
+    # Assuming you have created a warped binary image called "binary_warped"
+    binary_warped = img
+    # Assuming you have created a warped binary image called "binary_warped"
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[int(binary_warped.shape[0] / 2):, :], axis=0)
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0] / 2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0] / nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 58
+    # Set minimum number of pixels found to recenter window
+    minpix = 65
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window + 1) * window_height
+        win_y_high = binary_warped.shape[0] - window * window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        # cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high),
+        #               (0, 255, 0), 2)
+        # cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high),
+        #               (0, 255, 0), 2)
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                          (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                           (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    return left_fit, right_fit
+
+def curvature(warped_size, left_fit, right_fit):
+    ploty = np.linspace(0, warped_size[0] - 1, warped_size[0])
+    left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+    right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+    ym_per_pix = 30 / 720  # meters per pixel in y dimension
+    xm_per_pix = 3.7 / 700  # meters per pixel in x dimension
+    y_eval = np.max(ploty)
+    ploty = np.linspace(0, warped.shape[0] - 1, warped.shape[0])
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(ploty * ym_per_pix, left_fitx * xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty * ym_per_pix, right_fitx * xm_per_pix, 2)
+    left_curverad = ((1 + (2 * left_fit_cr[0] * y_eval * ym_per_pix + left_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
+        2 * left_fit_cr[0])
+    right_curverad = ((1 + (2 * right_fit_cr[0] * y_eval * ym_per_pix + right_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
+        2 * right_fit_cr[0])
+    curv = np.mean((left_curverad, right_curverad))
+    # print(left_curverad, 'm', right_curverad, 'm', curv, 'm')
+    return curv
+
+image = mpimg.imread('./test_images/test3.jpg')
 img_size = [image.shape[1], image.shape[0]]
-dist_pickle = './dist_pickle.p'
-with open(dist_pickle, mode='wb') as f:
+img_shape = (image.shape[0], image.shape[1])
+# import Camera Calibration Parameters
+dist_pickle = "./wide_dist_pickle.p"
+with open(dist_pickle, mode="rb") as f:
     CalData = pickle.load(f)
-    mtx, dist = CalData['mtx'], CalData['dist']
+    mtx, dist = CalData["mtx"], CalData["dist"]
+src = np.float32(
+    [[(img_size[0] / 2) - 65, img_size[1] / 2 + 100],
+    [((img_size[0] / 6) + 70), img_size[1] - 60],
+    [(img_size[0] * 5 / 6) - 40, img_size[1] - 60],
+    [(img_size[0] / 2 + 70), img_size[1] / 2 + 100]])
+dst = np.float32(
+    [[(img_size[0] / 4) + 75, 0],
+     [(img_size[0] / 4) + 75, img_size[1] - 60],
+     [(img_size[0] * 3/4) - 5, img_size[1] - 60],
+     [(img_size[0] * 3/4) - 5, 0]])
 
 
-plt.imshow(result)
+undist_img = Undistort(image, mtx, dist)
+binary = pipeline(undist_img)
+warped = warper(binary, img_size, src, dst)
+left_fit, right_fit = SlidingWindow(warped)
+line_curv = curvature(img_shape, left_fit, right_fit)
+
+
+plt.figure(figsize=(12,5))
+plt.subplot(131)
+plt.imshow(undist_img)
+plt.subplot(132)
+plt.imshow(binary, cmap='gray')
+draw_lines(src)
+plt.subplot(133)
+plt.imshow(warped, cmap='gray')
+draw_lines(dst)
